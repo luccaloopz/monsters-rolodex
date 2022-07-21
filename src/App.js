cce73d5bbc6f5e45e6b6c4fdd3cfe8a4361d2a6c
luccaloopz <luccamartins2012@gmail.com>
2022-07-21 13:32:58 -0700

    using setState on App.js by passing in an object

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: {firstName: 'Lucca', lastName: 'Martins'},
+      company: 'ZTM'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+          <button onClick={() => {
+            this.setState({ name: {firstName: 'John', lastName: 'Martins'} });  //updates state, sees that it's now a new object in memory and thus updates the app's UI
+            console.log(this.state)
+          }} >Change Name</button>
         </header>
       </div>
     );
