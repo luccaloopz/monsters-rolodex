ef14258115ebcc0c3733989728d56b6f47f0b84b
luccaloopz <luccamartins2012@gmail.com>
2022-07-29 17:00:57 -0700

    refactored card-list, card, and search-box components to be functional instead of class

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,19 +1,11 @@
-import { Component } from "react";
-
 import './card.styles.css';
 
-class Card extends Component {
-    render() {
-        const { id, name, email } = this.props;
-         
-        return (
-            <div className='card-container' key={id}>
-                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        )
-    }
-}
+const Card = ({monster}) => (
+    <div className='card-container' key={monster.id}>
+        <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
+    </div>
+)
 
 export default Card;
--- src/components/search-box/search-box.component.js
+++ src/components/search-box/search-box.component.js
@@ -1,18 +1,12 @@
-import { Component } from "react";
-
 import './search-box.styles.css';
 
-class SearchBox extends Component{
-    render() {
-        return(
-            <input 
-                className={`search-box ${this.props.className}`} 
-                type='search' 
-                placeholder={this.props.placeholder} 
-                onChange={this.props.onSearchChange}
-            /> 
-        )
-    }
-}
+const SearchBox = ({onSearchChange, className, placeholder}) => (
+    <input 
+        className={`search-box ${className}`} 
+        type='search' 
+        placeholder={placeholder} 
+        onChange={onSearchChange}
+    /> 
+)
 
 export default SearchBox;
